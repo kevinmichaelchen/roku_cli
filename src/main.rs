4e94886875c2d977aa688ee724a9ce9bd8440f34
Kevin Chen <kevinmichaelchen@gmail.com>
2022-12-17 19:47:10 -0500

    Roku buttons

--- src/main.rs
+++ src/main.rs
@@ -1,43 +1,9 @@
-#![allow(unused)]
+mod args;
 
-use anyhow::{Context, Result};
+use args::RokuArgs;
 use clap::Parser;
-use std::fs::File;
-use std::io::BufReader;
-use std::io::{self, BufRead};
-use std::path::Path;
 
-/// Search for a pattern in a file and display the lines that contain it.
-#[derive(Parser)]
-struct Cli {
-    /// The pattern to look for
-    pattern: String,
-    /// The path to the file to read
-    path: std::path::PathBuf,
-}
-
-fn main() -> Result<(), Box<dyn std::error::Error>> {
-    let args = Cli::parse();
-
-    let lines = read_lines(&args.path)
-        .with_context(|| format!("could not read file `{}`", &args.path.display()))?;
-
-    for line in lines {
-        let ip: String = line?;
-        if ip.contains(&args.pattern) {
-            println!("{}", ip);
-        }
-    }
-
-    Ok(())
-}
-
-// The output is wrapped in a Result to allow matching on errors
-// Returns an Iterator to the Reader of the lines of the file.
-fn read_lines<P>(filename: P) -> io::Result<io::Lines<io::BufReader<File>>>
-where
-    P: AsRef<Path>,
-{
-    let file = File::open(filename)?;
-    Ok(io::BufReader::new(file).lines())
+fn main() {
+    let args = RokuArgs::parse();
+    println!("{:?}", args);
 }
